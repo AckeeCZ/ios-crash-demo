e1894a5959d9f5c73de304f558b6974fd5081e96
Jakub Olejník <olejnjak@gmail.com>
2017-06-01 13:43:30 +0200

    Empty compileable app

---
+++ Source/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  crash-reporting-demo
+//
+//  Created by Jakub Olejník on 01/06/2017.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
